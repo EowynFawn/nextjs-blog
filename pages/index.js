cf446741b776e6dbb1e892aef1866c3abc63b21a
eowyn_p <cherryames96@gmail.com>
2021-04-22 13:45:22 -0500

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,32 @@
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Hey friend! I'm Éowyn. I'm a front-end developer and writer.
+          I believe in seeking beauty, and building it wherever I go.</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  )
+}
